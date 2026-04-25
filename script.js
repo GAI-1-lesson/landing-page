e72e364f179022fe324f6325a86eb1ef7a3468a2
GAI-1-lesson <seojaehyuk5012@gmail.com>
2026-04-25 17:08:31 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+// Scroll Reveal Animation
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
+
+// Navbar Background Change on Scroll
+window.addEventListener("scroll", function() {
+    var nav = document.querySelector("nav");
+    if (window.scrollY > 50) {
+        nav.classList.add("scrolled");
+    } else {
+        nav.classList.remove("scrolled");
+    }
+});
+
+// Initial Reveal on Load
+window.addEventListener("load", reveal);
+
+// Smooth Scrolling for Nav Links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
